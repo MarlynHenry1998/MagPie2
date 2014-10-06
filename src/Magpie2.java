862285e6a9d952b95a961ba0418f7a039a1e5ea3
MarlynHenry1998 <mimehenry@yahoo.com>
2014-10-06 11:02:42 -0400

    Magpie2
    added authors

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -7,7 +7,7 @@
  * 		    Handles responding to simple words and phrases 
  * </li></ul>
  * This version uses a nested if to handle default responses.
- * @author Laurie White
+ * @author Lauren White, Marlyn Henry, Sarah Ye
  * @version April 2012
  */
 public class Magpie2
