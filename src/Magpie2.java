c22b8fe8915d7787491be223d9843596087a5ac8
MarlynHenry1998 <mimehenry@yahoo.com>
2014-10-06 11:33:27 -0400

    Magpie2
    did activity 2

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -10,44 +10,52 @@
  * @author Lauren White, Marlyn Henry, Sarah Ye
  * @version April 2012
  */
-public class Magpie2
-{
-	/**
-	 * Get a default greeting 	
-	 * @return a greeting
-	 */
-	public String getGreeting()
-	{
-		return "Hello, let's talk.";
-	}
-	
-	/**
-	 * Gives a response to a user statement
-	 * 
-	 * @param statement
-	 *            the user statement
-	 * @return a response based on the rules given
-	 */
-	public String getResponse(String statement)
-	{
-		String response = "";
-		if (statement.indexOf("no") >= 0)
-		{
-			response = "Why so negative?";
-		}
-		else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
-		{
-			response = "Tell me more about your family.";
-		}
-		else
+public class Magpie2 {
+    /**
+     * Get a default greeting
+     *
+     * @return a greeting
+     */
+    public String getGreeting() {
+        return "Hello, let's talk.";
+    }
+
+    /**
+     * Gives a response to a user statement
+     *
+     * @param statement the user statement
+     * @return a response based on the rules given
+     */
+    public String getResponse(String statement) {
+        String response = "";
+        if (statement.indexOf("no") >= 0) {
+            response = "Why so negative?";
+        } else if (statement.indexOf("mother") >= 0
+                || statement.indexOf("father") >= 0
+                || statement.indexOf("sister") >= 0
+                || statement.indexOf("brother") >= 0) {
+            response = "Tell me more about your family.";
+        } else if (statement.indexOf("pet") >= 0
+                || statement.indexOf("cat") >= 0
+                || statement.indexOf("dog") >= 0) {
+            response = "Tell me more about your pets.";
+        } else if (statement.indexOf("Mr.") >=0) {
+            response = "He sounds like a good teacher.";
+        } else if (statement.indexOf("") >=0) {
+            response = "Say something please.";
+        } else if (statement.indexOf("food") >=0) {
+            response = "I am very hungry.";
+        } else if (statement.indexOf("death") >=0) {
+            response = "Don't be so depressing.";
+        } else if (statement.indexOf("diabetes") >=0) {
+            response = "Don't forget to take your insulin.";
+        }
+         else
 		{
 			response = getRandomResponse();
 		}
 		return response;
-	}
+    }
 
 	/**
 	 * Pick a default response to use if nothing else fits.
@@ -76,6 +84,14 @@ public class Magpie2
 		{
 			response = "You don't say.";
 		}
+        else if (whichResponse == 4)
+        {
+            response = "You're dumb.";
+        }
+        else if (whichResponse == 5)
+        {
+            response = "Life is a beautiful thing.";
+        }
 
 		return response;
 	}
